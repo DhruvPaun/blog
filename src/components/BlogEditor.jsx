ff3753b56fc48b8903ad18e389418dc0264fba1f
DhruvPaun <dhruvpaun28@gmail.com>
2025-12-02 19:52:25 +0530

    Maintenance

--- src/components/BlogEditor.jsx
+++ src/components/BlogEditor.jsx
@@ -4,7 +4,7 @@ import { Editor } from '@tinymce/tinymce-react'
 function BlogEditor({control,name,defaultValue=""}) {
   return (
     <Controller name={name||"content"}  control={control} render={({field:{onChange}})=>(
-        <Editor apiKey='0epo2ifa1s6r78z7iapr84v3b2i2jatq4o25xhsmab7dcjh8' initialValue={defaultValue} init={{
+        <Editor apiKey='b35590acabc2676a0f39' initialValue={defaultValue} init={{
             plugins: [
           'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
           'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'uploadcare', 'mentions', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
