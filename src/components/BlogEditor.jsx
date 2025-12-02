611ef46147f59be4f85d4f34af3fec4abe8607b3
DhruvPaun <dhruvpaun28@gmail.com>
2025-12-02 21:45:30 +0530

    changes

--- src/components/BlogEditor.jsx
+++ src/components/BlogEditor.jsx
@@ -6,10 +6,6 @@ function BlogEditor({control,name,defaultValue=""}) {
   return (
     <Controller name={name||"content"}  control={control} render={({field:{onChange}})=>(
         <Editor apiKey='0epo2ifa1s6r78z7iapr84v3b2i2jatq4o25xhsmab7dcjh8' initialValue={defaultValue} init={{
-            plugins: [
-          'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-          'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'uploadcare', 'mentions', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
-        ],
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
         }} onEditorChange={onChange}/>
     )}/>
